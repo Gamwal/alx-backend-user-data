44e3a408bbfb14b1d3996d80d257866e65e3a434
Gamaliel Adun <adungamaliel@gmail.com>
2023-11-07 09:28:56 +0100

    added templates for Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,13 +16,13 @@ class Auth:
         public method require_auth
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """
         public method authorization_header
         """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """
         public method current_user
